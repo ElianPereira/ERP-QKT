bff7cab1c988e07920794c12b25035c2fb422393
Elian <quintakooxtanil@gmail.com>
2026-01-14 11:13:02 -0600

    Módulo de recibos PDF funcionando

--- core_erp/urls.py
+++ core_erp/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from comercial.views import ver_recibo  # <--- Importamos la vista que acabamos de crear
+# CORRECCIÓN: Importamos el nombre real de tu función
+from comercial.views import generar_pdf_cotizacion 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Esta es la nueva ruta: ejemplo.com/recibo/1
-    path('recibo/<int:id_cotizacion>/', ver_recibo, name='ver_recibo'),
+    
+    # Ruta para el PDF
+    path('cotizacion/<int:cotizacion_id>/pdf/', generar_pdf_cotizacion, name='cotizacion_pdf'),
 ]
