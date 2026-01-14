0f1e3b97fc7182921bf57589b8ff7cd72b1ddfde
Elian <quintakooxtanil@gmail.com>
2026-01-14 12:12:55 -0600

    Botón de imprimir recibo en admin

--- comercial/admin.py
+++ comercial/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html  # <--- Necesario para el HTML del botón
+from django.urls import reverse            # <--- Necesario para buscar la URL
 from .models import Insumo, Producto, ComponenteProducto, Cliente, Cotizacion, Pago
 
 # Esto permite agregar insumos DENTRO de la pantalla de crear producto
@@ -13,9 +15,24 @@ class ProductoAdmin(admin.ModelAdmin):
 
 @admin.register(Cotizacion)
 class CotizacionAdmin(admin.ModelAdmin):
-    list_display = ('cliente', 'producto', 'fecha_evento', 'estado', 'precio_final', 'saldo_pendiente')
+    # Agregamos 'boton_imprimir' al final de la lista
+    list_display = ('cliente', 'producto', 'fecha_evento', 'estado', 'precio_final', 'saldo_pendiente', 'boton_imprimir')
     list_filter = ('estado', 'fecha_evento')
     search_fields = ('cliente__nombre',)
+    
+    # Opcional: Para que el botón aparezca también al entrar a editar la cotización
+    readonly_fields = ('boton_imprimir',)
+
+    # --- LÓGICA DEL BOTÓN ---
+    def boton_imprimir(self, obj):
+        if obj.id:
+            # Buscamos la URL por el nombre que le pusimos en urls.py ('cotizacion_pdf')
+            url = reverse('cotizacion_pdf', args=[obj.id])
+            # Creamos el botón HTML (usamos la clase 'button' de Django para que se vea bonito)
+            return format_html('<a class="button" href="{}" target="_blank" style="background-color: #447e9b; color: white; padding: 3px 10px; border-radius: 5px;">🖨️ Imprimir Recibo</a>', url)
+        return "Guardar primero"
+
+    boton_imprimir.short_description = "Acciones" # Título de la columna
 
 @admin.register(Pago)
 class PagoAdmin(admin.ModelAdmin):
